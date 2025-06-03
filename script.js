3548a216661ea9ec4bdeb8ce7a681c0373c26709
Di16anka <diana.pasekova@rambler.ru>
2025-06-03 21:59:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const list = document.quareSelector('.item');
+const typing = document.quareSelector ('#input');
+
+typing.addEventListener ('keydown', function(event) {
+  const text = typing.value;
+  event.key=='Enter';
+
+ const newText = document.createElement('div');
+  newText.classList.add('groceries');
+  newText.textContent = text;
+  if (text !='') {
+  list.append(newText);
+  }
+    typing.value='';
+ 
+});
+    
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
