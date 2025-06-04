66c867e5cd520ae8c8c00f1ac24f37ebb679f697
Di16anka <diana.pasekova@rambler.ru>
2025-06-04 20:38:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const shpoppingList =  [];
 const purchase = document.querySelector('#input');
-const list = document.querySelector ('.item');
+const list = document.querySelector('.item');
 
 purchase.onclick = function(event) {
       if (event.key == 'Enter') {
     };
-        f
-        const toDo = document.createElement('li');
-
+purchase.addEventListener ('click' , addItem); 
+function addItem() {
+    const itemName = purchase.value;
+    if (itemName !== '') {
+       purchase.value = '';
+           
+    }      
+      
+     const toDo = document.createElement('li');
+      const text = purchase.value;
+      toDo.textContent = text;
+      toDo.addEventListener ('click' , function () {
+      toDo.classList.add('done');
+            list.append (toDo);
+      });
+     
+            
+            
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
